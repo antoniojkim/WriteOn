cce95ab963ee4e073c0c977e9e0596831af411c0
paul <pli283@uwo.ca>
2018-02-24 18:35:32 -0500

    added some html files

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,27 @@
+var wow;
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
     document.getElementById("input").innerHTML = selection[0];
+    wow = selection[0];
 });
+
+
+function show_more(){
+	var x = document.getElementById("more");
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    } else {
+        x.style.display = "none"
+    }
+}
+document.getElementById('showmore').addEventListener('click', show_more);
+
+
+
+function b1(){
+	console.log(wow)
+	somethingreplace(wow);
+
+}
+document.getElementById('b1').addEventListener('click', b1);
