9cdb6f878ebdecc4809d6fb6998bb6827a0f34d6
GarlandZhang <garland._zhang@hotmail.com>
2018-02-24 09:00:31 -0500

    replaced text

--- src/js/contextMenuScript.js
+++ src/js/contextMenuScript.js
@@ -1,9 +1,28 @@
+var sel = window.getSelection();
+var range = sel.getRangeAt(0); 
+var rect = range.getBoundingClientRect();
 var dialogBox = document.createElement("DIALOG");
 dialogBox.style.color = "rgb(155, 102, 102)";
 dialogBox.style.position = 'absolute';
-dialogBox.style.right = null;
-dialogBox.style.left = 110;
+alert(rect.left.toString());
+alert(rect.top.toString());
+alert(rect.right);
+alert(rect.bottom.toString());
+
+dialogBox.style.right = ((rect.right / window.innerWidth) * 100) + '%';
+dialogBox.style.bottom = ((rect.bottom / window.innerHeight) * 100) + '%';
 dialogBox.setAttribute("open", "open");
+
+
+var selectionContents = range.extractContents();
+var div = document.createElement("div");
+var text = document.createTextNode("JESSUSSS");
+div.appendChild(text);
+div.appendChild(selectionContents);
+range.insertNode(div);
+
+
+
 //get text values
 var arr = ["text1", "text2", "text3", "text4", "text5"];
 var t = [];
@@ -17,23 +36,23 @@ for(var i = 0; i < arr.length; i++){
 }
 
 elem[0].addEventListener("click", function () {
-    
+    alert("clicked on text1!");
 });
 
 elem[1].addEventListener("click", function () {
-    
+    alert("clicked on text2!");
 });
 
 elem[2].addEventListener("click", function () {
-    
+    alert("clicked on text3!");
 });
 
 elem[3].addEventListener("click", function () {
-    
+    alert("clicked on text4!");
 });
 
 elem[4].addEventListener("click", function () {
-    
+    alert("clicked on text5!");
 });
 
 
